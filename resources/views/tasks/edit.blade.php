582fab68bce8fe6df1df2c5796f11959ee4ec6b6
fumiya.niikura <gitgitgiton@gmail.com>
2018-05-29 06:36:47 +0000

    modified edit.blade.php

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -4,9 +4,9 @@
 
        <h1>id: {{ $task->id }}のタスク編集ページ</h1> 
        
-       {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'task' => 'put']) !!}
+       {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
        
-              {!! Form::label('content', 'タスク') !!}
+              {!! Form::label('content', 'タスク:') !!}
               {!! Form::text('content') !!}
               
               {!! Form::submit('更新') !!}
