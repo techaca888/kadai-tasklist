0015c3a9eea6329eef9c3e18114b2b3a05f211de
fumiya.niikura <gitgitgiton@gmail.com>
2018-05-30 02:57:59 +0000

    apply bootstrap

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -4,16 +4,24 @@
 
     <h1>タスク新規追加ページ</h1>
     
+    <div class="row">
+    <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
+        
     {!! Form::model($task, ['route' => 'tasks.store']) !!}
     
+      <div class="form-group">
         {!! Form::label('content', 'タスク:') !!}
-        {!! Form::text('content')  !!}
-        
+        {!! Form::text('content', null,  ['class' => 'form-control'])  !!}
+      </div>
+      
+      <div class="form-group">
         {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::select('status', array('Complete' => 'Complete', 'Incomplete' => 'Incomplete'), 'Incomplete'); !!}
-        
-        {!! Form::submit('追加') !!}
+        {!! Form::select('status', ['Complete' => 'Complete', 'Incomplete' => 'Incomplete'], 'Incomplete', ['class' => 'form-control']) !!}
+      </div>    
+        {!! Form::submit('追加', ['class' => 'btn btn-primary']) !!}
     
     {!! Form::close() !!}
 
+    </div>
+    </div>
 @endsection
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -4,17 +4,22 @@
 
        <h1>id: {{ $task->id }}のタスク編集ページ</h1> 
        
+       <div class="row">
+       <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
        {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
-       
+         <div class="form-group">
               {!! Form::label('content', 'タスク:') !!}
-              {!! Form::text('content') !!}
-              
-              
+              {!! Form::text('content', null, ['class' => 'form-control']) !!}
+         </div>
+          
+          
+         <div class="form-group">
               {!! Form::label('status', 'ステータス:') !!}
-              {!! Form::select('status', array('Complete' => 'Complete', 'Incomplete' => 'Incomplete'), 'Incomplete'); !!}
-              
+              {!! Form::select('status', ['Complete' => 'Complete', 'Incomplete' => 'Incomplete'], 'Incomplete', ['class' => 'form-control']) !!}
+         </div>
               {!! Form::submit('更新') !!}
        
        {!! Form::close() !!}
-
+       </div>
+       </div>
 @endsection
